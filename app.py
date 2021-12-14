c0ef3fab3c74246c2c7db905b185f791a182a2e4
Naveen Kumar Konam <NaveenKumarKonam@NaveenKumars-MacBook-Pro.local>
2021-12-14 21:19:40 +0530

    deleted some files

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, request, redirect
-from flask.globals import g
 from flask.helpers import url_for
 from data import cement_plants_data
-import geopandas
-
-
-
 
 app = Flask(__name__)
 
@@ -25,24 +20,11 @@ def map():
     df = cement_plants_data()
     group = sorted(list(set(df['Group'])))
     state = sorted(list(set(df['State'])))
-    gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude))
-    return render_template("map.html", group=group,state = state, gdf = gdf)
+    return render_template("map.html", group=group,state = state)
 
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
-
-@app.route("/contact", methods=['GET','POST'])
-def contact():
-    return render_template("contact.html")
-
-@app.route("/checkmap",methods=['GET', 'POST'])
-def checkmap():
-    if request.method=="post":
-        first_name = request.form.get("fname")
-        last_name = request.form.get("lname") 
-        return (first_name, last_name)
-    return render_template("checkmap.html")
     
 if __name__ == '__main__':
     app.run(debug=True)
--- data.py
+++ data.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 
 def cement_plants_data():
-    url = '~/Desktop/factoryquest/FactoryQuest/cementplants_sample.csv'
     url_latest = './static/Cement_Plants_in_India.csv'
     df = pd.read_csv(url_latest, encoding = "ISO-8859-1", engine='python')
     return(df)
